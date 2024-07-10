43d787db64fc1a7d8dc5fa1b1bff7825a86e026c
Asmita Nalawade <asmita.nalawade@miovision.com>
2024-07-11 00:53:01 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+
 // Using React.createElement we can give 3 arguments 
 // 1 - Element Tag
 // 2 - {} provides attribute like class, id, style etc.
 // 3 - content
 // React.createElement is an object ===> HTML(browser undertsnad)
 
-const heading = React.createElement("h1", {}, "Hello World using React!!");
-console.log(heading); // Object
+// const heading = React.createElement("h1", {}, "Hello World using React!!");
+// console.log(heading); // Object
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);  //render() is used here to get the heading object and convert it in html, put it in DOM which browser understand and pass in root.
+// root.render(heading);  //render() is used here to get the heading object and convert it in html, put it in DOM which browser understand and pass in root.
 
 // Example 2: Below code write in React
 
@@ -32,6 +35,7 @@ React.createElement("div", {className: "child1"},
 [React.createElement('h1', {}, "I'm h1 tag"), React.createElement('h2', {}, "I'm h2 tag")])]
 )
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent); // In each component there is only one render on root. If we add two render the it render the last content only.
 
